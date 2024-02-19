540367ebbb35408e09c1562ef48d71512f9b3707
Simão Neto <simaomenezesneto@gmail.com>
2024-02-19 22:22:38 +0000

    feat: Work with properties

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -2,6 +2,16 @@ import {Post} from "./Post";
 
 export function App() {
   return (
-    <Post/>
+    <div>
+      <Post 
+        author="Simon Peter" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore, nam reprehenderit officia excepturi mollitia. Dignissimos deserunt minima dolore qui nisi quas assumenda ullam consectetur delectus sapiente. Culpa, sequi deleniti." 
+      />
+
+      <Post 
+        author="Mathew Peter" 
+        content="Today I learning about reactjs" 
+      />
+    </div>
   )
 }
