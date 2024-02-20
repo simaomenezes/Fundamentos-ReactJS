e773ff315e9ce51245f51f79ed915c9d50490e53
Simão Neto <simaomenezesneto@gmail.com>
2024-02-20 23:33:55 +0000

    feat: Created component Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,29 @@
 import {Post} from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header/>
 
-      <Post 
-        author="Simon Peter" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore, nam reprehenderit officia excepturi mollitia. Dignissimos deserunt minima dolore qui nisi quas assumenda ullam consectetur delectus sapiente. Culpa, sequi deleniti." 
-      />
-
-      <Post 
-        author="Mathew Peter" 
-        content="Today I learning about reactjs" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author="Simon Peter" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore, nam reprehenderit officia excepturi mollitia. Dignissimos deserunt minima dolore qui nisi quas assumenda ullam consectetur delectus sapiente. Culpa, sequi deleniti." 
+          />
+          <Post 
+            author="Mathew Peter" 
+            content="Today I learning about reactjs" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
