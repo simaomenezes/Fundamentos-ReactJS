7260087aa2f3919a6d8c18b4cb1351224c57cbd1
Simão Neto <simaomenezesneto@gmail.com>
2024-02-20 23:01:47 +0000

    feat: Work CSS global

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,13 @@
 import {Post} from "./Post";
 import { Header } from "./components/Header";
 
-import './styles.css'
+import './global.css'
 
 export function App() {
   return (
     <div>
       <Header/>
-      
+
       <Post 
         author="Simon Peter" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore, nam reprehenderit officia excepturi mollitia. Dignissimos deserunt minima dolore qui nisi quas assumenda ullam consectetur delectus sapiente. Culpa, sequi deleniti." 
