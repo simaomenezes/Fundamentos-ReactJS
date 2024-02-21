a54542427dd3352daa57641d9bbf2c1b67aae89c
Simão Neto <simaomenezesneto@gmail.com>
2024-02-21 20:56:33 +0000

    feat: Create form of comments

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -25,6 +25,19 @@ export function Post(){
                     <a href=''>#gamer</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+
         </article>
     )
 }
